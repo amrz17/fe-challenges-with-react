b9cb6e2ef0e98d74206b89d8c8c70198fa5de6e5
amrz17 <muhammad.amer45@gmail.com>
2024-01-13 22:58:41 +0700

    Add Side Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,57 @@
 import { logo, avatar } from "../assets/images";
-import { iconCartNav, iconMenu } from "../assets/icons";
+import { iconCartNav, iconClose, iconMenu } from "../assets/icons";
 import { navLink } from "../constants";
 import { DropDown } from "./DropDown";
 import { useState } from "react";
-import { SideBar } from "./SideBar";
 
 export const Nav = () => {
   const [openCart, setOpenCart] = useState(false);
-  const [menu, setMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <header className="w-full absolute z-10 p-4">
-      <nav className="flex justify-center items-center  mx-auto max-container w-full">
+      <nav
+        className="flex justify-center items-center  
+      mx-auto max-container w-full"
+      >
         <div className="flex gap-4 mr-10 md:mr-48">
-          <div className="flex lg:hidden">
-            <a
-              href="/"
+          <div className="flex lg:hidden gap-6">
+            <button
               onClick={(e) => {
                 e.preventDefault();
-                setMenu((prev) => !prev);
+                setOpenMenu((openMenu) => !openMenu);
               }}
             >
               <img src={iconMenu} alt="icon-menu" width={30} />
+            </button>
+            {openMenu && (
+              <div
+                className="flex w-3/5 absolute left-0 top-0 
+                bg-white h-screen"
+              >
+                <ul className="pl-6">
+                  <button className="mt-4 mb-8">
+                    <img
+                      src={iconClose}
+                      width={30}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setOpenMenu((openMenu) => !openMenu);
+                      }}
+                    />
+                  </button>
+                  {navLink.map((link) => (
+                    <li key={link.label} className="mb-6 text-xl font-bold">
+                      <a href={link.href}>{link.label}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+            <a href="/">
+              <img src={logo} alt="logo" width={150} />
             </a>
           </div>
-          <a href="/">
-            <img src={logo} alt="logo" width={150} />
-          </a>
         </div>
         <ul className="hidden lg:flex text-base text-dark-grayish-blue flex-1 justify-start items-center gap-10 ml-14">
           {navLink.map((item) => (
@@ -42,17 +67,11 @@ export const Nav = () => {
           href="/"
           onClick={(e) => {
             e.preventDefault();
-            setOpenCart((prev) => !prev);
+            setOpenCart((openCart) => !openCart);
           }}
           className="flex justify-end mx-4 lg:mx-7"
         >
-          <img
-            className="text-black bg-black"
-            src={iconCartNav}
-            alt="icon-cart-nav"
-            width={25}
-            height={25}
-          />
+          <img src={iconCartNav} alt="icon-cart-nav" width={25} height={25} />
         </a>
         <a href="/" className="flex justify-end lg:mx-3">
           <img
@@ -65,7 +84,6 @@ export const Nav = () => {
       </nav>
       <hr className="hidden mx-auto lg:flex justify-center items-center w-10/12 mt-7" />
       {openCart && <DropDown />}
-      {menu && <SideBar />}
     </header>
   );
 };
